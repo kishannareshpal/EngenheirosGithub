208b0d882a12e73cdb513822d4ffd677e3dc0756
Kishan Jadav <kishan_jadav@hotmail.com>
2018-02-17 09:49:25 +0200

    fixed an aditional test on a cadeira name 4o ano

--- 4oAno/anosSupl.js
+++ 4oAno/anosSupl.js
@@ -1,4 +1,4 @@
-var cadeiras1oSemNAMES = ["Estruturas Metálicas e de Madeira", "Gestão de Qualidade", "Instalações em Edifícios I", "Antropologia Cultural de Moçambique", "Edificações", "Betão IIestruturas"]
+var cadeiras1oSemNAMES = ["Estruturas Metálicas e de Madeira", "Gestão de Qualidade", "Instalações em Edifícios I", "Antropologia Cultural de Moçambique", "Edificações", "Betão II"]
 var cadeiras1oSemIDS = ["estruturas", "gestao", "instalacoes", "antropologia", "edificacoes", "betao2"]
 
 var cadeiras2oSemNAMES = [""]
