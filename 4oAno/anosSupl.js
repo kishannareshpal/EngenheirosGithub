f05c0c3feece9201dda6ce6bfae3f4ac31acc1c7
Kishan Jadav <kishan_jadav@hotmail.com>
2018-02-17 22:31:47 +0200

    temporary fix

--- 4oAno/anosSupl.js
+++ 4oAno/anosSupl.js
@@ -1,8 +1,23 @@
+var cadeiras1oSemIDS_vias = ["betao2", "estruturas", "gqc", "viasferreas", "viasrodoviarias1", "antropologia"]
+var cadeiras2oSemIDS_vias = ["drenagemviaria", "trafego", "pontes", "projecto", "praticas", "viasrodoviarias2"]
+var cadeiras1oSemIDS_obras = ["betao2", "recursoshidricos", "gqc", "hidrogeologia", "hidrometria", "hidroquimica", "antropologia"]
+var cadeiras2oSemIDS_obras = ["grh", "hidrurbana", "irrigacaodrenagem", "ofm", "praticas", "projecto"]
+var cadeiras1oSemIDS_edificios = ["betao2", "edificacoes", "estruturas", "gqc", "instalacoes1", "antropologia"]
+var cadeiras2oSemIDS_edificios = ["instalacoes2", "patologia", "praticas", "projconstrucao", "reabilitacao", "tecnicanaoconvecional"]
+
+var cadeiras1oSemNAMES_vias = ["Betão II", "Estruturas Metálicas e de Madeira", "Gestão de Qualidade na Construção", "Vias Férreas", "Vias Rodoviárias I", "TT: Antropologia Cultural de Moçambique"]
+var cadeiras2oSemNAMES_vias = ["Drenagem Viária", "Engenharia de Tráfego", "Pontes", "Projecto Viário", "Práticas de Estaleiro e de Obras de Vias", "Vias Rodoviárias II"]
+var cadeiras1oSemNAMES_obras = ["Betão II", "Engenharia de Recursos Hídricos", "Gestão da Qualidade na Construção", "Hidrogeologia", "Hidrometria", "Hidroquímica", "TT: Antropologia Cultural de Moçambique"]
+var cadeiras2oSemNAMES_obras = ["Gestão de Recursos Hídricos", "Hidráulica Urbana", "Irrigação e Drenagem", "Obras Fluviais e Marítimas", "Práticas de Campo, Laboratório e Obras", "Projecto em Construção Hidráulicas"]
+var cadeiras1oSemNAMES_edificios = ["Betão II", "Edificações", "Estruturas Metálicas e de Madeira", "Gestão de Qualidade na Construção", "Instalações em Edifícios I", "TT: Antropologia Cultural de Moçambique"]
+var cadeiras2oSemNAMES_edificios = ["Instalações em Edifícios II", "Patologia em Edifícios", "Práticas de Estaleiro e Obras de Edifícios", "Projecto de Construção de Edifícios", "Reabilitação de Estruturas e Materiais", "Técnica de Construçõa Não-Convencional"]
+
+
 var cadeirasObject = {
   "1oSem": {
     "edificios": {
       "names": ["Betão II", "Edificações", "Estruturas Metálicas e de Madeira", "Gestão de Qualidade na Construção", "Instalações em Edifícios I", "TT: Antropologia Cultural de Moçambique"],
-      "id": ["betao2", "edificacoes", "estruturas", "gqc", "instalacoes1", "antropologia"]
+      "id":    ["betao2", "edificacoes", "estruturas", "gqc", "instalacoes1", "antropologia"]
     },
 
     "obrashidraulicas": {
